22b28190d15e934e8fdb3a27019be62e81897372
water <water34545@yandex.ru>
2021-05-05 18:10:16 +0300

    start magic game

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Magic Game Start</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
